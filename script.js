acb466e619534151a3d2e0583e6c3272710ed476
UUD-dev <uudvideogames@gmail.com>
2026-01-22 21:04:44 +1030

    update

--- script.js
+++ script.js
@@ -1,420 +1,183 @@
+const websocketport = 8080;
+const websockethost = "127.0.0.1";
 
-const websocketport = 8080
-const websockethost = '127.0.0.1'
-var ignoreList = []
-///////////////////////////////////////
-//CONNECTING TO THE STREAMER.BOT CLIENT
-///////////////////////////////////////
+let ignoreList = [];
 
+/* =====================
+   STREAMER.BOT CLIENT
+===================== */
 const client = new StreamerbotClient({
+  host: websockethost,
+  port: websocketport,
+  endpoint: "/",
 
-    //sicne we are hosting the file locally we dont need an ip, leave as 'localhost' or '127.0.0.1'
-    host: websockethost,
+  onError: (err) => {
+    console.error("ERROR", err);
+    appendMessage({
+      html: formatAlert(`[ERROR] ${JSON.stringify(err)}`),
+      timeout: 10
+    });
+  },
 
-    //the port we used in Streamer.bot's websocket settings.
-    port: websocketport, 
+  onConnect: async () => {
+    appendMessage({
+      html: formatAlert("[CONNECTED] Client connected successfully (v0.4.1)"),
+      timeout: 10
+    });
 
-    //dont change
-    endpoint: '/', 
+    const excluded = await updateExcluded();
+    appendMessage({
+      html: formatAlert(`[UPDATE] Ignorelist: ${excluded.join(", ")}`),
+      timeout: 10
+    });
 
-    onError: (err) => {
-        console.log("ERROR\m",err)
-        let message = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage">[ERROR!] ${JSON.stringify(err)}</span></b></span>
-        `
-        displayTemporaryMessage(message)
-    },
-    onConnect: async (data) => {
-        let message = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage">[CONNECTED] Client connected succesfully! (v0.4.3)</span></b></span>
-        `
-        displayTemporaryMessage(message)
-        let excludedList = await updateExcluded()
-        let ignoreListOutput = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage">[UPDATE] Ignorelist : ${excludedList}</span></b></span>
-        `
-        displayTemporaryMessage(ignoreListOutput)
-
-        setInterval(async() => {
-            await updateExcluded()
-        }, 1000*60*5);
-    }
+    setInterval(updateExcluded, 1000 * 60 * 5);
+  }
 });
 
-////////////////////////
-//SUBSCRIPTION FUNCTIONS
-////////////////////////
+/* =====================
+   EVENT HANDLERS
+===================== */
 
-//This function runs when we detect a twitch chat message has been sent.
-client.on('Twitch.ChatMessage', (data) => {
-    
-    //set the username of the message sender.
-    let username = data.data.user.name
+client.on("Twitch.ChatMessage", ({ data }) => {
+  const username = data.user.name;
+  if (shouldIgnore(username)) return;
 
-    //check to see if we should ignore this message
-    for (let user in ignoreList){ 
-        let ignoreUsername = username.toString().toLowerCase()
-        let ignoreCheckname = ignoreList[user].toString().toLowerCase()
-        console.log("CHECKING",ignoreUsername,"VS",ignoreCheckname)
-        if (ignoreUsername == ignoreCheckname){ 
-            console.log("IGNOREING USER:",username)
-            return 
-        }
-    }
-
-    //set the message of the message sender.
-    let message = data.data.user.message
-
-    //creating the message string EXAMPLE (   [User1234]: Hello stream!   )
-    let messageString = `[${username}]: ${data.data.message.message}` 
-
-    //send the message to youtube.
-    sendYoutubeMessage(messageString)    
-    
-    // Send the chat data to our function to handle displaying twitch messages in the overlay.
-    displayTwitchChatMessage(data.data)	
-            
-});           
-//This function runs when we detect a youtube chat message has been sent.
-client.on('YouTube.Message', (data) => {
-    
-    //set the username of the message sender.
-    let username = data.data.user.name
-
-    //check to see if we should ignore this message
-    for (let user in ignoreList){ 
-        let ignoreUsername = username.toString().toLowerCase()
-        let ignoreCheckname = ignoreList[user].toString().toLowerCase()
-        console.log("CHECKING",ignoreUsername,"VS",ignoreCheckname)
-        if (ignoreUsername == ignoreCheckname){ 
-            console.log("IGNOREING USER:",username)
-            return 
-        }
-    }
-
-    //set the message of the message sender.
-    let message = data.data.user.message
-    
-    //creating the message string EXAMPLE (   [User1234]: Hello stream!   )
-    let messageString = `[${username}]: ${data.data.message}`
-    
-    //send the message to twitch.
-    sendTwitchMessage(messageString)
-    
-    // Send the chat data to our function to handle displaying youtube messages in the overlay.
-    displayYoutubeChatMessage(data.data)	
-                            
+  sendYoutubeMessage(`[${username}]: ${data.message.message}`);
+  renderChatMessage({
+    platform: "twitch",
+    username: data.message.displayName,
+    message: data.message.message,
+    first: data.message.firstMessage,
+    highlighted: data.message.isHighlighted
+  });
 });
 
-client.on('YouTube.NewSubscriber', ({ event, data }) => {
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
-    //set the username of the message sender.
-    let username = data.data.user.name
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage">${username} Just subscribed on YouTube!</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
+client.on("YouTube.Message", ({ data }) => {
+  const username = data.user.name;
+  if (shouldIgnore(username)) return;
 
-    
+  sendTwitchMessage(`[${username}]: ${data.message}`);
+  renderChatMessage({
+    platform: "youtube",
+    username: data.user.name,
+    message: data.message
+  });
 });
 
-client.on('YouTube.SuperChat', ({ event, data }) => {
+client.on("Twitch.Follow", ({ data }) =>
+  appendMessage({ html: formatAlert(`${data.user_name} followed on Twitch`) })
+);
 
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
+client.on("YouTube.NewSubscriber", ({ data }) =>
+  appendMessage({ html: formatAlert(`${data.data.user.name} subscribed on YouTube`) })
+);
 
-    let username = data.data.user.name
-    let message = data.data.message.message
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertSuperchat">[SUPERCHAT] ${username} : ${message}</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
-});
+client.on("Twitch.Cheer", ({ data }) =>
+  appendMessage({
+    html: formatAlert(`${data.user.name} cheered ${data.bits} bits (${data.message})`)
+  })
+);
 
-client.on('Twitch.Follow', ({ event, data }) => {
+/* =====================
+   MESSAGE RENDERING
+===================== */
 
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
+function renderChatMessage({ platform, username, message, first, highlighted }) {
+  const classes = [];
+  if (first) classes.push("firstmessage");
+  if (highlighted) classes.push("highlighted");
 
-    let username = data.user_name
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertFollow">${username} Just Followed on Twitch!</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
-});
-
-client.on('Twitch.Cheer', ({ event, data }) => {
-
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
-
-    let username = data.user.name
-    let bits = data.bits
-    let message = data.message
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertCheer">${username} Just cheered ${bits}Bits! (${message})</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
-
-});
-
-client.on('Twitch.CoinCheer', ({ event, data }) => {
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
-
-    let username = data.user.name
-    let bits = data.bits
-    let message = data.message
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertCheer">${username} Just cheered ${bits}Bits! (${message})</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
-});
-
-client.on('Twitch.GiftBomb', ({ event, data }) => {
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
-
-    let giftReceivers = data.recipients
-    for (i in giftReceivers){
-        let username = i['name']
-        let alertMessage = 
-            `
-            <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertCheer">${username} Received a gifted sub!</span></b></span>
-            `
-        displayAlertMessage(alertMessage)
-    }
-
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertCheer">${data.user} gave out ${giftReceivers.length} Gifted Subs!</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
-});
-
-client.on('Twitch.GiftSub', ({ event, data }) => {
-
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
-
-    let receiver = data.recipient.name
-    let gifter = data.user.name
-
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertCheer">${receiver} Just scored a gifted sub from! ${gifter}</span></b></span>
-        `
-
-    displayAlertMessage(alertMessage)
-
-});
-
-client.on('Twitch.ReSub', ({ event, data }) => {
-
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
-
-    let username = data.user.name
-    let subLength = data.user.monthsSubscribed
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertCheer">${username} Just re-Subscribed! (${subLength} months)</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
-
-});
-
-client.on('Twitch.RewardRedemption', ({ event, data }) => {
-
-    console.log('Received event:', event.source, event.type);
-    console.log('Event data:', data);
-
-    let username = data.user.name
-    let subLength = data.user.monthsSubscribed
-    let alertMessage = 
-        `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage alertCheer">${username} Just re-Subscribed! (${subLength} months)</span></b></span>
-        `
-    displayAlertMessage(alertMessage)
-});
-
-///////////////////
-//MESSAGE FUNCTIONS
-///////////////////
-
-async function sendTwitchMessage(message){
-    await client.doAction(action = {name:"sendMessageToTwitch"}, args = {message:message});
+  appendMessage({
+    classes,
+    html:
+      `<span class="message">` +
+      `<b>${platformIcon(platform)}<span class="username">${escapeHtml(username)}</span>:</b> ` +
+      `${escapeHtml(message)}` +
+      `</span>`
+  });
 }
 
-async function sendYoutubeMessage(message){
-    await client.doAction(action = {name:"sendMessageToYoutube"}, args = {message:message});
+function appendMessage({ html, classes = [], timeout = 0 }) {
+  const div = document.createElement("div");
+  div.id = generateMessageId();
+  div.className = "chat-message " + classes.join(" ");
+  div.innerHTML = html;
+
+  const chatBox = document.getElementById("messages");
+  chatBox.appendChild(div);
+  chatBox.scrollTop = chatBox.scrollHeight;
+
+  if (timeout > 0) {
+    setTimeout(() => div.remove(), timeout * 1000);
+  }
 }
 
-function displayTwitchChatMessage(data) {
+/* =====================
+   HELPERS
+===================== */
 
-    let newMessageDiv = document.createElement('div');
-    let messageId = generateMessageId()
-
-    let firstMessage = data.message.firstMessage
-    let isHighlighted = data.message.isHighlighted
-
-    //check if this is a first time user!
-    if (firstMessage){
-        firstMessage = "firstMessage"
-    }else{
-        firstMessage = ""
-    }
-
-    if (isHighlighted){
-        isHighlighted = "highlighted"
-    }else{
-        isHighlighted = ""
-    }
-
-    newMessageDiv.id = messageId
-
-    //Creates HTML string to display.
-    newMessageDiv.innerHTML = `
-    <span class="message ${firstMessage} ${isHighlighted}">
-        <b><img class="icon" src="images/twitch.png"></img> <span class="username">${data.message.displayName}</span>:</b>
-        ${data.message.message}</span>
-    
-    `;
-    newMessageDiv.className = 'chat-message'; 
-    var chatBox = document.getElementById('messages');
-    
-    chatBox.appendChild(newMessageDiv); 
-
-    if (data.message.subscriber){
-        document.getElementById(messageId).style.color = "#a304a8";	
-    }
-    if (data.message.isHighlighted){
-        document.getElementById(messageId).style.backgroundColor = "#a12da5a4";	
-    }
-
-    chatBox.scrollTop = chatBox.scrollHeight;
-    deleteMessage(messageId, 0)
-
+function platformIcon(platform) {
+  const src =
+    platform === "twitch"
+      ? "images/twitch.png"
+      : platform === "youtube"
+      ? "images/youtube.png"
+      : "images/alert.png";
+  return `<img class="icon" src="${src}"> `;
 }
 
-function displayYoutubeChatMessage(data) {
-
-    let newMessageDiv = document.createElement('div');
-    let messageId = generateMessageId()
-    newMessageDiv.id = messageId
-
-    newMessageDiv.innerHTML = `
-    <span class="message">
-        <b><img class="icon" src="images/youtube.png"></img> <span class="username">${data.user.name}</span>:</b>
-        ${data.message}</span>
-    
-    `;
-    newMessageDiv.className = 'chat-message'; 
-    var chatBox = document.getElementById('messages');
-
-    chatBox.appendChild(newMessageDiv);
-
-    if (data.message.subscriber){
-        document.getElementById(messageId).style.color = "#a304a8";	
-    }
-    if (data.message.isHighlighted){
-        document.getElementById(messageId).style.backgroundColor = "#a12da5a4";	
-    }
-
-    chatBox.scrollTop = chatBox.scrollHeight;
-    deleteMessage(messageId, 0)
-
+function formatAlert(text) {
+  return (
+    `<span class="message">` +
+    `<img class="icon" src="images/alert.png"> ` +
+    `<span class="alertMessage">${escapeHtml(text)}</span>` +
+    `</span>`
+  );
 }
 
-function displayAlertMessage(message) {
-
-    let newMessageDiv = document.createElement('div');
-    let messageId = generateMessageId()
-    newMessageDiv.id = messageId
-    newMessageDiv.innerHTML = `
-    <span class="message">${message}</span>`;
-
-    newMessageDiv.className = 'chat-message';
-    var chatBox = document.getElementById('messages');
-
-    chatBox.appendChild(newMessageDiv);
-    chatBox.scrollTop = chatBox.scrollHeight;
-
-    deleteMessage(messageId, 0)
-
-}
-
-function displayTemporaryMessage(message) {
-
-    let newMessageDiv = document.createElement('div');
-    let messageId = generateMessageId()
-    newMessageDiv.id = messageId
-    newMessageDiv.innerHTML = `
-    <span class="message">${message}</span>`;
-
-    newMessageDiv.className = 'chat-message';
-    var chatBox = document.getElementById('messages');
-
-    chatBox.appendChild(newMessageDiv);
-    chatBox.scrollTop = chatBox.scrollHeight;
-
-    deleteMessage(messageId, 10)
-
-}
-
-function deleteMessage(msgId, timeout){
-    if (timeout > 0){
-        setTimeout(() => {
-                const msgElement = document.getElementById(msgId);
-                if (msgElement) {
-                        console.log("REMOVING MESSAGE",msgId)
-                        msgElement.remove(); // Or msgElement.style.display = 'none'; to hide it
-                }
-        }, 1000 * timeout); // in seconds
-    }
-    
+function shouldIgnore(username) {
+  return ignoreList
+    .map(u => u.toLowerCase())
+    .includes(username.toLowerCase());
 }
 
 function generateMessageId() {
-    // Generate a random number between 0 and 16777215 (0xFFFFFF)
-    const randomInt = Math.floor(Math.random() * 16777215);
-    let hexColor = randomInt.toString(16);
-    hexColor = `#${hexColor.padStart(6, '0')}`;
-    return hexColor;
+  return "msg-" + Math.random().toString(36).slice(2, 10);
 }
 
-//////////////////
-//HELPER FUNCTIONS
-//////////////////
+function escapeHtml(text) {
+  return String(text)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+}
 
-async function updateExcluded(){
-    //reset the array
-    ignoreList = ['Streamelements']
-    // retreive the broadcaster information from Streamer.bot
-    const response = await client.getBroadcaster(); 
-    // console.log("response",response)
-    // console.log(response.platforms)
-    //This loops through all active streaming platforms (Twitch, YouTube)
-    for (let i in response.platforms){
+/* =====================
+   OUTGOING CHAT
+===================== */
 
-        if (response.platforms[i].botUserName){
-            ignoreList.push(response.platforms[i].botUserName)
-        }
+async function sendTwitchMessage(message) {
+  await client.doAction({ name: "sendMessageToTwitch" }, { message });
+}
 
-        if (response.platforms[i].broadcastUserName){
-            ignoreList.push(response.platforms[i].broadcastUserName)
-        }
-        
-    }
-    console.log("IGNORELIST : ",ignoreList)
-    return ignoreList
+async function sendYoutubeMessage(message) {
+  await client.doAction({ name: "sendMessageToYoutube" }, { message });
+}
+
+/* =====================
+   IGNORE LIST
+===================== */
+
+async function updateExcluded() {
+  ignoreList = ["streamelements"];
+
+  const response = await client.getBroadcaster();
+  for (const platform of response.platforms) {
+    if (platform.botUserName) ignoreList.push(platform.botUserName);
+    if (platform.broadcastUserName) ignoreList.push(platform.broadcastUserName);
+  }
+
+  console.log("IGNORELIST:", ignoreList);
+  return ignoreList;
 }
