31163e4097df57902d6d94dd174b51a17f3c9a1f
UUD-dev <uudvideogames@gmail.com>
2026-01-22 14:01:18 +1030

    5

--- stream_overlay/stream_overlay.js
+++ stream_overlay/stream_overlay.js
@@ -24,7 +24,7 @@ const client = new StreamerbotClient({
     onConnect: async (data) => {
         let message = 
         `
-        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage">[CONNECTED] Chat Overlay Connected (v0.3.4)</span></b></span>
+        <b><img class="icon" src="images/alert.png"></img> <span class="alertMessage">[CONNECTED] Chat Overlay Connected (v0.3.5)</span></b></span>
         `
         displayTemporaryMessage(message)
     }
